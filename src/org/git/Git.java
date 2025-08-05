f3247ff17eb5e88efc44c798715d42993a3ec88a
vijayalakshmidhanagopal <vijayalakshmi7400@gmail.com>
2025-08-05 11:28:49 +0530

    Add project

---
+++ src/org/git/Git.java
@@ -0,0 +1,17 @@
+package org.git;
+
+public class Git {
+	
+	private void hello() {
+		System.out.println("welcome");
+
+	}
+	public static void main(String[] args) {
+		Git obj = new Git();
+		obj.hello();
+		
+		
+		
+	}
+
+}
